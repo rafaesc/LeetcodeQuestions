462e6010ffefcf570a5487bf48e8a0f4f29ac257
unknown <rafaesc92@gmail.com>
2025-06-02 00:14:54 -0500

    Remove Duplicates from Sorted Array exercise

---
+++ Arrays/RemoveDuplicatesFromSortedArray.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    var k = 1;
+    var ref = 1;
+    var index = 1;
+    
+    for(var i = 1; i < nums.length ; i++) {
+        if (nums[i] !== nums[i-1]) {
+            nums[ref] = nums[i]
+            ref = ref+1;
+            k++;
+        }
+    }    
+    return k;
+};
