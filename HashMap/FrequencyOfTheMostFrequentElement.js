96a3d74992af0c56da203895c5d9a217be27535b
unknown <rafaesc92@gmail.com>
2025-06-29 21:49:40 -0500

    1838 Frequency of the Most Frequent Element

--- HashMap/FrequencyOfTheMostFrequentElement.js
+++ HashMap/FrequencyOfTheMostFrequentElement.js
@@ -1,1 +1,28 @@
-FrequencyOfTheMostFrequentElement.js
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxFrequency = function(nums, k) {
+    nums = nums.sort((a, b) => a - b)
+    
+    var left = 0;
+    var right = 0;
+    var maxFrequency = 0;
+    var total = 0;
+
+    while (right < nums.length) {
+        total += nums[right];
+
+        while (nums[right] * (right - left + 1) > total + k) {
+            total -= nums[left];
+            left++;
+        }
+        maxFrequency  = Math.max(maxFrequency, right - left + 1); 
+
+        right++;
+    }
+
+    return maxFrequency;
+
+};
