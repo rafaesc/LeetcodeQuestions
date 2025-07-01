5592a3bfac498f06b06c350ee412eeeccf6d1bb9
unknown <rafaesc92@gmail.com>
2025-06-30 23:24:09 -0500

    1. Two Sum

---
+++ HashMap/TwoSum.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var map = {};
+
+    for (var i = 0; i < nums.length; i++) {
+        var num = nums[i];
+
+        if (map[num] !== undefined) {
+            return [map[num], i]
+        }
+
+        var diff = target - num;
+        
+        map[diff] = i;
+
+    }
+
+};
